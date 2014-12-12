79389ef2da57c6eb187c08ce4ec61182a21eb92a
Daejun Park <daejunpark@gmail.com>
2014-12-12 16:51:52 -0600

    fix prelude modules

--- kbuild.sh
+++ kbuild.sh
@@ -4,4 +4,4 @@ dir=`dirname $0`
 
 echo "rule @PWD => \"`pwd`\"" >$dir/js-pwd.k
 $dir/kpp.py $dir/js-main.k >$dir/js.k
-$dir/k/bin/kompile --backend java $dir/js.k
+$dir/k/bin/kompile --no-prelude --backend java $dir/js.k
