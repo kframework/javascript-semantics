825953a78e4e2171fb4c513d97766fff3cbc9555
Daejun Park <daejunpark@gmail.com>
2015-02-05 11:45:35 -0600

    Update coverage.sh

--- test262-coverage/coverage.sh
+++ test262-coverage/coverage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for i in "`for i in $(cat stdlib-for-coverage.txt); do find ../test262/test/suite/ch15/ -name $i; done`"; do
-  ../run-test262.sh "$i"
+  cat ../prelude.js "$i" >"$i".prelude && \
+  ../jsmassage.sh -f "$i".prelude >"$i".prelude.massage && \
+  krun -d . --pattern-matching --coverage-file "$i".cov "$i".prelude.massage >"$i".out 2>"$i".err
 done
 for i in "`find ../test262 -name '*.cov'`"; do
   grep 'js.k' "$i" | cut -f 2 -d ':' >coverage
