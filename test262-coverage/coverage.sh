d0e32b4f501cdbd9cb380b90298c465b9d55c3ae
Daejun Park <daejunpark@gmail.com>
2015-02-06 00:17:10 -0600

    Update coverage script

--- test262-coverage/coverage.sh
+++ test262-coverage/coverage.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# echo "Running additional core tests in stdlib directory..."
-# for i in `for j in $(cat stdlib-for-coverage.txt); do find ../test262/test/suite/ch15/ -name $j; done`; do
-#   cat ../prelude.js "$i" >"$i".prelude && \
-#   ../jsmassage.sh -f "$i".prelude >"$i".prelude.massage && \
-#   krun -d . --pattern-matching --coverage-file "$i".cov "$i".prelude.massage >"$i".out 2>"$i".err
-# done
+# NOTE: Run first 'make -k -j N test262-core-coverage' where N is a number of processes.
+
 echo "Gathering trace information..."
 for i in `find ../test262 -name '*.cov'`; do grep 'js.k' "$i" | cut -f 2 -d ':'; done >coverage
 echo "Generating coverage report..."
