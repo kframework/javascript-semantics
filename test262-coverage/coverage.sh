e037b6e796859bbcea02c184399b959cf9fe6741
Daejun Park <daejunpark@gmail.com>
2015-02-07 01:22:54 -0600

    Update coverage.sh

--- test262-coverage/coverage.sh
+++ test262-coverage/coverage.sh
@@ -5,4 +5,7 @@
 echo "Gathering trace information..."
 for i in `find ../test262 -name '*.cov'`; do grep 'js.k' "$i" | cut -f 2 -d ':'; done >coverage
 echo "Generating coverage report..."
-./report.py coverage ../js.k >coverage.k
+./report.py coverage ../js.k | \
+sed 's/^[ ]\([ ]*0[ ][ ]*\)\(rule\|Let\|Do\|If\|Return\|Nop\|Exit\)/!\1\2/' | \
+sed 's/^\([ ][ ]*\)0/\1 /' | \
+sed 's/^!/ /' >coverage.k
