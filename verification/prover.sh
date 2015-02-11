ca65f7fd8dd7deb7ef7fbee7c4cfd8157a7267a2
Daejun Park <daejunpark@gmail.com>
2015-02-10 23:12:48 -0600

    reorganize verification directory structure

--- verification/prover.sh
+++ verification/prover.sh
@@ -12,10 +12,14 @@ set -x
 # Basic
 # "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/min_spec.k                  "$dir"/min.js
 
+# List in C-like style
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/list --smt none                                            --prove "$dir"/list/reverse_spec.k        "$dir"/list/reverse.js 
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/list --smt none                                            --prove "$dir"/list/append_spec.k         "$dir"/list/append.js
+
 # BST in C-like style
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/string_find_spec.k      "$dir"/bst/find.js 
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/string_insert_spec.k    "$dir"/bst/insert.js 
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/string_delete_spec.k    "$dir"/bst/delete.js 
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/tree_string --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/string_find_spec.k      "$dir"/bst/find.js 
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/tree_string --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/string_insert_spec.k    "$dir"/bst/insert.js 
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/tree_string --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/string_delete_spec.k    "$dir"/bst/delete.js 
 
 # BST in OOP style
 # "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/bst_find_spec.k         "$dir"/bst/bst3.js 
@@ -23,10 +27,6 @@ set -x
 # "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/bst/bst_delete_spec.k       "$dir"/bst/bst3.js 
 
 # AVL in C-like style
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/avl/avl_find_spec.k         "$dir"/avl/avl.js 
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/avl/avl_insert_spec.k       "$dir"/avl/avl.js 
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/avl/avl_delete_spec.k       "$dir"/avl/avl.js 
-
-# List in C-like style
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/list/reverse_spec.k        "$dir"/list/reverse.js 
-  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/../ --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/list/append_spec.k         "$dir"/list/append.js
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/tree_string --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/avl/avl_find_spec.k         "$dir"/avl/avl.js 
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/tree_string --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/avl/avl_insert_spec.k       "$dir"/avl/avl.js 
+  "$k"/k-distribution/target/release/k/bin/krun -v --debug -d "$dir"/patterns/tree_string --smt_prelude "$k"/k-distribution/include/z3/string.smt2 --prove "$dir"/avl/avl_delete_spec.k       "$dir"/avl/avl.js 
